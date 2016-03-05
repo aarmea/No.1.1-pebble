c3374709da121426ac625bd338147cd216d0c60b
Albert Armea <albert@albertarmea.com>
2016-03-04 23:27:14 -0800

    Add a basic "date" setting to the settings page

---
+++ settings_src/settings.js
@@ -0,0 +1,32 @@
+const DEFAULT_SUBMIT_URL = 'pebblejs://close#';
+
+window.onload = function() {
+  var submitButton = document.getElementById('submitButton');
+  submitButton.addEventListener('click', onSubmit);
+};
+
+function onSubmit() {
+  var config = {
+    'showDate': document.getElementById('showDate').checked
+  };
+
+  var submitUrl = getQueryParam('return_to', DEFAULT_SUBMIT_URL);
+  location.href = submitUrl + encodeURIComponent(JSON.stringify(config));
+}
+
+// Get query variables
+// Pasted from https://developer.pebble.com/guides/pebble-apps/pebblekit-js/app-configuration/
+function getQueryParam(variable, defaultValue) {
+  // Find all URL parameters
+  var query = location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+
+    // If the query variable parameter is found, decode it to use and return it for use
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return defaultValue || false;
+}
