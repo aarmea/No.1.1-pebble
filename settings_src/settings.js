995b5e5d02ab73c6023a4130379d0232ed575fe1
Albert Armea <albert@albertarmea.com>
2016-05-14 13:07:56 -0700

    Make the settings page look less bad

--- settings_src/settings.js
+++ settings_src/settings.js
@@ -41,3 +41,16 @@ function getQueryParam(variable, defaultValue) {
   }
   return defaultValue || false;
 }
+
+// Google fonts
+WebFontConfig = {
+  google: { families: [ 'Ek+Mukta::latin' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
